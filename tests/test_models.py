25ac6a654035e72750d5c5db4eeeaf4687296133
Bjørn Lindi <bjornlin@dhcp-10-24-56-219.wlan.ntnu.no>
2022-09-07 15:30:21 +0200

    Add more tests
    - test_daily_max(), using an array of length 3
    - test_daily_min(), using negative integers and array of length 3

--- tests/test_models.py
+++ tests/test_models.py
@@ -49,3 +49,22 @@ def test_daily_minimum_integers():
     test_result = np.array([1, 2])
     # Need to use Numpu testing function to compare arrays
     npt.assert_array_equal(daily_min(test_input), test_result)
+
+
+def test_daily_max():
+    """Test that max function works for an array of positive integers"""
+    from inflammation.models import daily_max
+
+    test_input = np.array([[4, 2, 5],
+                           [1, 6, 2],
+                           [4, 1, 9]])
+    test_result = np.array([4, 6, 9])
+def test_daily_min():
+    """Test that min function works for an array of positive and negative integers"""
+    from inflammation.models import daily_min
+
+    test_input = np.array([[ 4, -2, 5],
+                           [1, -6, 2],
+                           [-4, -1, 9]])
+    test_result = np.array([-4, -6, 2])
+    npt.assert_array_equal(daily_min(test_input), test_result)
